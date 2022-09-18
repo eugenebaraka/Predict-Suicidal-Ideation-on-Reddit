2fe1e638d081197f3380bfb0a8d080531945d8de
Eugene Baraka <eugenebaraka@gmail.com>
2022-09-17 20:48:52 -0400

    add readme

---
+++ linkedin-ds-career-journey/README.md
@@ -0,0 +1,14 @@
+## Question
+For Canadians, is higher education in data science/computer science correlated with: 
+- quick promotion as a data scientist?
+- company tenure?
+
+## Answer 
+To answer this question, I need to scrape linkedin data for canadian data scientists. Here's the main info I need:
+- Five most recent positions
+- Company cities
+- Company tenure
+- Education
+- Certificates
+
+Desired size of dataset: >10k
--- linkedin-ds-career-journey/scraper.py
+++ linkedin-ds-career-journey/scraper.py
@@ -1,5 +1,4 @@
-
-
+## Import necessary libraries
 import time
 import parsel
 import csv
@@ -10,29 +9,45 @@ from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+## Initialize chrome webdriver 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
 driver.get('https://www.linkedin.com')
 
-sleep(3)
+sleep(2)    ## I use sleep throughout the script to allow for webpage loading lags
 
+## Login to a linkedin account
 username = driver.find_element(By.ID, 'session_key')
-username.send_keys("email@email.com")
+username.send_keys("eugenebaraka@gmail.com")
 
-sleep(3)
+sleep(2)
 
 password = driver.find_element(By.ID, 'session_password')
-password.send_keys('XXXXX')
+password.send_keys('Tuamini128.')
 
-sleep(3)
+sleep(2)
 
 login = driver.find_element(By.CLASS_NAME, 'sign-in-form__submit-button')
 login.click()
 
 sleep(15)
 
-search_term = driver.find_element(By.CLASS_NAME, 'global-nav-search')
+search_term = driver.find_element(By.ID, "ember28")
 search_term.send_keys("Data Scientist")
+search_term.send_keys(Keys.RETURN)
 
-sleep(15)
+# # sleep(15)
 
+# ## Search profiles using google
+
+
+
+
+
+# search_profile = driver.find_element(By.CLASS_NAME, 'q')
+# linkedin_urls = driver.find_element(By.CSS_SELECTOR, '.r a')
+# linkedin_urls = [url.get_attribute('href') for url in linkedin_urls]
+
+# for profile in linkedin_urls:
+#         search_profile.send_keys("profile AND 'Data Scientist'")
+#         search_profile.send_keys(Keys.RETURN)

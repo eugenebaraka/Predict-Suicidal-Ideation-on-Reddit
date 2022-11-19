f5c16b866f1c28801e933c97b3505287221d7d42
Eugene Baraka <eugenebaraka@gmail.com>
2022-11-19 18:36:52 -0500

    edits on utls

--- predict suicidal ideation/utils.py
+++ predict suicidal ideation/utils.py
@@ -32,109 +32,117 @@ import copy as cp
 
 ## Feature Extraction
 
-### NER (would be better to write a class for this??)
-
-def ner_displacy(txt, ner=None, lst_tag_filter=None, title=None, serve=False):
-    ner = spacy.load("en_core_web_lg") if ner is None else ner
-    doc = ner(txt)
-    doc.user_data["title"] = title
-    if serve == True:
-        spacy.displacy.serve(doc, style="ent", options={"ents":lst_tag_filter})
-    else:
-        spacy.displacy.render(doc, style="ent", options={"ents":lst_tag_filter})
+# def ner_displacy(txt, ner=None, lst_tag_filter=None, title=None, serve=False):
+#     ner = spacy.load("en_core_web_lg") if ner is None else ner
+#     doc = ner(txt)
+#     doc.user_data["title"] = title
+#     if serve == True:
+#         spacy.displacy.serve(doc, style="ent", options={"ents":lst_tag_filter})
+#     else:
+#         spacy.displacy.render(doc, style="ent", options={"ents":lst_tag_filter})
 
 
-def utils_ner_text(txt, ner=None, lst_tag_filter=None, grams_join="_"):
-    ## apply model
-    ner = spacy.load("en_core_web_lg") if ner is None else ner
-    entities = ner(txt).ents
+# def utils_ner_text(txt, ner=None, lst_tag_filter=None, grams_join="_"):
+#     ## apply model
+#     ner = spacy.load("en_core_web_lg") if ner is None else ner
+#     entities = ner(txt).ents
 
-    ## tag text
-    tagged_txt = txt
-    for tag in entities:
-        if (lst_tag_filter is None) or (tag.label_ in lst_tag_filter):
-            try:
-                tagged_txt = re.sub(tag.text, grams_join.join(tag.text.split()), tagged_txt) #it breaks with wild characters like *+
-            except Exception as e:
-                continue
+#     ## tag text
+#     tagged_txt = txt
+#     for tag in entities:
+#         if (lst_tag_filter is None) or (tag.label_ in lst_tag_filter):
+#             try:
+#                 tagged_txt = re.sub(tag.text, grams_join.join(tag.text.split()), tagged_txt) #it breaks with wild characters like *+
+#             except Exception as e:
+#                 continue
 
-    ## extract tags list
-    if lst_tag_filter is None:
-        lst_tags = [(tag.text, tag.label_) for tag in entities]  #list(set([(word.text, word.label_) for word in ner(x).ents]))
-    else: 
-        lst_tags = [(word.text, word.label_) for word in entities if word.label_ in lst_tag_filter]
+#     ## extract tags list
+#     if lst_tag_filter is None:
+#         lst_tags = [(tag.text, tag.label_) for tag in entities]  #list(set([(word.text, word.label_) for word in ner(x).ents]))
+#     else: 
+#         lst_tags = [(word.text, word.label_) for word in entities if word.label_ in lst_tag_filter]
 
-    return tagged_txt, lst_tags
+#     return tagged_txt, lst_tags
         
         
-def utils_lst_count(lst, top=None):
-    dic_counter = collections.Counter()
-    for x in lst:
-        dic_counter[x] += 1
-    dic_counter = collections.OrderedDict(sorted(dic_counter.items(), key=lambda x: x[1], reverse=True))
-    lst_top = [ {key:value} for key,value in dic_counter.items() ]
-    if top is not None:
-        lst_top = lst_top[:top]
-    return lst_top
+# def utils_lst_count(lst, top=None):
+#     dic_counter = collections.Counter()
+#     for x in lst:
+#         dic_counter[x] += 1
+#     dic_counter = collections.OrderedDict(sorted(dic_counter.items(), key=lambda x: x[1], reverse=True))
+#     lst_top = [ {key:value} for key,value in dic_counter.items() ]
+#     if top is not None:
+#         lst_top = lst_top[:top]
+#     return lst_top
 
-def utils_ner_features(lst_dics_tuples, tag):
-    if len(lst_dics_tuples) > 0:
-        tag_type = []
-        for dic_tuples in lst_dics_tuples:
-            for tuple in dic_tuples:
-                type, n = tuple[1], dic_tuples[tuple]
-                tag_type = tag_type + [type]*n
-                dic_counter = collections.Counter()
-                for x in tag_type:
-                    dic_counter[x] += 1
-        return dic_counter[tag]   #pd.DataFrame([dic_counter])
-    else:
-        return 0
+# def utils_ner_features(lst_dics_tuples, tag):
+#     if len(lst_dics_tuples) > 0:
+#         tag_type = []
+#         for dic_tuples in lst_dics_tuples:
+#             for tuple in dic_tuples:
+#                 type, n = tuple[1], dic_tuples[tuple]
+#                 tag_type = tag_type + [type]*n
+#                 dic_counter = collections.Counter()
+#                 for x in tag_type:
+#                     dic_counter[x] += 1
+#         return dic_counter[tag]   #pd.DataFrame([dic_counter])
+#     else:
+#         return 0
 
 
-def add_ner_spacy(dtf, column, ner=None, lst_tag_filter=None, grams_join="_", create_features=True):
-    tqdm.pandas()
-    ner = spacy.load("en_core_web_lg") if ner is None else ner
-    ## tag text and exctract tags
-    print("--- tagging ---")
-    dtf[[column+"_tagged", "tags"]] = dtf[[column]].progress_apply(lambda x: utils_ner_text(x[0], ner, lst_tag_filter, grams_join), 
-                                                          axis=1, result_type='expand')
+# def add_ner_spacy(dtf, column, ner=None, lst_tag_filter=None, grams_join="_", create_features=True):
+#     tqdm.pandas()
+#     ner = spacy.load("en_core_web_lg") if ner is None else ner
+#     ## tag text and exctract tags
+#     print("Tagging...")
+#     dtf[[column+"_tagged", "tags"]] = dtf[[column]].progress_apply(lambda x: utils_ner_text(x[0], ner, lst_tag_filter, grams_join), 
+#                                                           axis=1, result_type='expand')
 
-    ## put all tags in a column
-    print("--- counting tags ---")
-    dtf["tags"] = dtf["tags"].progress_apply(lambda x: utils_lst_count(x, top=None))
+#     ## put all tags in a column
+#     print("Tags counting...")
+#     dtf["tags"] = dtf["tags"].progress_apply(lambda x: utils_lst_count(x, top=None))
     
-    ## extract features
-    if create_features == True:
-        print("--- creating features ---")
-        ### features set
-        tags_set = []
-        for lst in dtf["tags"].tolist():
-            for dic in lst:
-                for k in dic.keys():
-                    tags_set.append(k[1])
-        tags_set = list(set(tags_set))
-        ### create columns
-        for feature in tags_set:
-            dtf["tags_"+feature] = dtf["tags"].progress_apply(lambda x: utils_ner_features(x, feature))
-    return dtf
+#     ## extract features
+#     if create_features == True:
+#         print("Creating features...")
+#         ### features set
+#         tags_set = []
+#         for lst in dtf["tags"].tolist():
+#             for dic in lst:
+#                 for k in dic.keys():
+#                     tags_set.append(k[1])
+#         tags_set = list(set(tags_set))
+#         ### create columns
+#         for feature in tags_set:
+#             dtf["tags_"+feature] = dtf["tags"].progress_apply(lambda x: utils_ner_features(x, feature))
+#     return dtf
 
-def plot_tags(tags, top=30, figsize=(10,5)):   
-    tags_list = tags.sum()
-    map_lst = list(map(lambda x: list(x.keys())[0], tags_list))
-    dtf_tags = pd.DataFrame(map_lst, columns=['tag','type'])
-    dtf_tags["count"] = 1
-    dtf_tags = dtf_tags.groupby(['type','tag']).count().reset_index().sort_values("count", ascending=False)
-    fig, ax = plt.subplots(figsize=figsize)
-    fig.suptitle("Top frequent tags", fontsize=12)
-    sns.barplot(x="count", y="tag", hue="type", data=dtf_tags.iloc[:top,:], dodge=False, ax=ax)
-    ax.set(ylabel=None)
-    ax.grid(axis="x")
-    plt.show()
-    return dtf_tags
+# def plot_tags(tags, top=30, figsize=(10,5)):   
+#     tags_list = tags.sum()
+#     map_lst = list(map(lambda x: list(x.keys())[0], tags_list))
+#     dtf_tags = pd.DataFrame(map_lst, columns=['tag','type'])
+#     dtf_tags["count"] = 1
+#     dtf_tags = dtf_tags.groupby(['type','tag']).count().reset_index().sort_values("count", ascending=False)
+#     fig, ax = plt.subplots(figsize=figsize)
+#     fig.suptitle("Top frequent tags", fontsize=12)
+#     sns.barplot(x="count", y="tag", hue="type", data=dtf_tags.iloc[:top,:], dodge=False, ax=ax)
+#     ax.set(ylabel=None)
+#     ax.grid(axis="x")
+#     plt.show()
+#     return dtf_tags
 
 def extract_lengths(data, col):
+    """Extract word, character, sentence, average word length, and 
+    average sentence length for each row.
     
+    Arguments:
+        data (DataFrame): dataframe name
+        col             : column name
+
+    Returns: 
+        new_data (DataFrame): data with appended length variables
+    
+    """
     tqdm.pandas()
     new_data = data.copy()
     new_data['word_count'] = new_data[col].progress_apply(lambda x: len(nltk.word_tokenize(str(x))))
@@ -153,7 +161,26 @@ def extract_lengths(data, col):
 ## Univariate and Bivariate Visualizations
 
 def plot_distributions(data, x, y = None, maxcat = 20, top_n = None, bins = None, figsize = (15, 9), title = None, xlabel = None, ylabel = None, normalize = True):
+    """ Plot distributions of a variable.
+
+    Arguments:
+        data (DataFrame): dataframe name
+        x               : column to display on the x axis
+        y               : column to display on the y axix
+        maxcat (int.)   : maximum number of categories to be in the x axis column
+        top_n (int.)    : number of categories to plot
+        bins (int.)     : number of bins in the barplot
+        figsize (tuple) : size of the figure
+        title (str.)    : title of the figure
+        xlabel (str.)   : x axis label
+        ylabel (str.)   : y axis label
+        normalize (bool): whether to normalize x axis (by percentage) or not
+
+    Returns:
+        plot (matplotlib): histogram or barplot 
     
+    """
+
     # Univariate Analysis
     if y is None: 
         fig, ax = plt.subplots(figsize = figsize)
@@ -192,6 +219,16 @@ def plot_distributions(data, x, y = None, maxcat = 20, top_n = None, bins = None
 # D. Create stopwords
 
 def stopwords_list(langs = ['english'], add_words = [], remove_words = []):
+    """ List of stopwords to remove from the text.
+
+    Arguments:
+        langs (list): list of languages
+        add_words (list): list of words to add
+        remove_words (list): list of words to remove
+
+    Returns:
+        list_stopwords (list): sorted list of stopwords
+    """
     
     for lang in langs:
         list_stopwords = set(nltk.corpus.stopwords.words(lang))
@@ -205,6 +242,18 @@ def stopwords_list(langs = ['english'], add_words = [], remove_words = []):
 # G. Text Preprocessing
 
 def text_preprocessing(txt, rmv_regex = None,lower=True, stopwords = None):
+    """Remove punctuations, numbers, stopwords, expand contractions, 
+    convert text to lower case, and tokenize.
+
+    Arguments:
+        txt (string): text to be processed
+        rmv_regex   : regex for patterns to remove from text
+        lower (bool): whether to convert all text to lower case or not
+        stopwords (list): stopwords to remove from txt
+
+    Returns:
+        txt (list): list of tokenized strings
+    """
     if rmv_regex is not None:
         for regex in rmv_regex:
             txt = re.sub(regex, "", txt)
@@ -226,6 +275,18 @@ def text_preprocessing(txt, rmv_regex = None,lower=True, stopwords = None):
     return txt
     
 def append_clean_text(data, column, rmv_regex = None, lower = True, stopwords = None, remove_na=True):
+    """ Append cleaned text to the original dataset.
+    Arguments:
+        data (DataFrame): dataframe name
+        column          : column name
+        rmv_regex       : regex for pattern to be removed from text
+        lower (bool)    : whether to convert all text to lower case or not
+        stopwords (list): stopwords to remove from txt
+        remove_na (bool): whether to remove NAs from the text
+
+    Returns:
+        dtf (DataFrame) : dataframe with appended clean text column
+    """
     dtf = data.copy()
     tqdm.pandas()
     ## apply preprocess
@@ -244,6 +305,17 @@ def append_clean_text(data, column, rmv_regex = None, lower = True, stopwords = 
 
 ## Sentiment Analysis
 def add_sentiment(data, column, algo="vader", sentiment_range=(-1,1)):
+    """Calculate sentiment analysis for text and add a column to the dataset.
+    
+    Arguments:
+        data (DataFrame): dataframe name
+        column          : column name
+        algo (str.)     : sentiment analysis algorithm name
+        sentiment_range : range of numbers for sentiment
+
+    Returns: 
+        dtf (DataFrame) : dataframe with appended clean text column
+    """
     dtf = data.copy()
     tqdm.pandas()
     ## calculate sentiment
@@ -260,6 +332,17 @@ def add_sentiment(data, column, algo="vader", sentiment_range=(-1,1)):
 
 ## Word Frequency
 def word_freq(corpus, ngrams=[1,2,3], top=10, figsize=(10,7)):
+    """Find how frequent words are in the text and plot the frequency.
+    
+    Arguments:
+        corpus (str.): text to evaluate word frequency on
+        ngrams (list): number of n-grams to look for in corpus
+        top (int.)   : number of top words to plot
+        figsize (tuple): figure size
+
+    Returns: 
+        dtf_freq (DataFrame): dataframe containing frequency of words in each corpus
+        """
     lst_tokens = nltk.tokenize.word_tokenize(corpus.str.cat(sep=" "))
     ngrams = [ngrams] if type(ngrams) is int else ngrams
     
@@ -283,6 +366,17 @@ def word_freq(corpus, ngrams=[1,2,3], top=10, figsize=(10,7)):
     return dtf_freq
 
 def plot_wordcloud(corpus, max_words=150, max_font_size=35, figsize=(10,10)):
+    """Plot word cloud for a corpus.
+    
+    Arguments: 
+        corpus (str.)       : text to plot word cloud for
+        max_words (int.)    : exlude words as freqent as this
+        max_font_size (int.): the font size for the most frequent word
+        figsize             : figure size
+
+    Returns: 
+        plot (matplotlib): wordcloud plot
+    """
     wc = wordcloud.WordCloud(background_color='black', max_words=max_words, max_font_size=max_font_size)
     wc = wc.generate(str(corpus)) #if type(corpus) is not dict else wc.generate_from_frequencies(corpus)     
     fig = plt.figure(num=1, figsize=figsize)
@@ -294,6 +388,20 @@ def plot_wordcloud(corpus, max_words=150, max_font_size=35, figsize=(10,10)):
 ## Modeling using bag of words
 
 def bow(X_train, X_test, vectorizer = None, top = 20, show_top = False): 
+    """Generate bag of words and plot top n words.
+    
+    Arguments: 
+        X_train (DataFrame): unlabelled training data
+        X_test (DataFrame): unlabelled validation set
+        top (int.)        : number of words to plot
+        show_top (bool)   : whether to plot words or not
+
+    Returns:    
+        X_train_transformed (DataFrame): transformed unlabelled training data 
+        X_test_transformed (DataFrame) : tranformed unlabelled test set
+
+    """
+
     # vectorize 
     print("Creating sparse matrices...")
     stemmer = nltk.stem.porter.PorterStemmer() 
@@ -321,7 +429,6 @@ def bow(X_train, X_test, vectorizer = None, top = 20, show_top = False):
 
 
 ## Cross validation confusion matrix
-
 
 def cross_validation_(model, kfold : model_selection.KFold, X : np.array, y : np.array) -> Tuple[np.array, np.array, np.array]:
 
